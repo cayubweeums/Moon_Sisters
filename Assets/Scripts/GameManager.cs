c2aac8303906b76151f8137c391fcb7b0038afaa
Mitchell <mlrotenberry99@yahoo.com>
2021-02-24 17:05:41 -0600

    Created start menu. Added script to allow travel between scenes.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class GameManager : MonoBehaviour
     public GameObject dialogBox;
     public GameObject dialogText;
     private Coroutine dialogCoroutine;
+    public GameObject startButton;
+    public GameObject StartBackground;
+    public GameObject canvas;
+    public GameObject events;
+
 
 
 
@@ -29,16 +36,59 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(events);
         }
         else
         {
             Destroy(gameObject);
+            Destroy(events);
+            Destroy(canvas);
         }
     }
 
     public static GameManager Instance
     {
         get; private set;
+    }
+
+    public void StartButton()
+    {
+        startButton.SetActive(false);
+        StartCoroutine(LoadYourAsyncScene("FirstScene"));
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        StartCoroutine(ColorLerp(new Color(1, 1, 1, 0), 1f));
+    }
+
+    IEnumerator ColorLerp(Color endValue, float duration)
+    {
+        float time = 0;
+        Image sprite = StartBackground.GetComponent<Image>();
+        Color startValue = sprite.color;
+
+        while (time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        sprite.color = endValue;
     }
 
     public void StartDialog(string text)
