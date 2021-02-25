3cb648eebacb77f8de9ad16865b87a4fdfc4d99e
Mitchell <mlrotenberry99@yahoo.com>
2021-02-25 00:31:16 -0600

    Messed with follow cam

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -8,25 +8,30 @@ public class FollowCam : MonoBehaviour
     public GameObject player;
 
     public float boundaryPercent;
-    public float easing;
+    public float easing = 0.1f;
 
+    private Camera myCam;
     private float lBound, rBound, uBound, dBound;
 
     // Start is called before the first frame update
     void Start()
     {
-        lBound = boundaryPercent * Camera.main.pixelWidth;
-        rBound = Camera.main.pixelWidth - lBound;
-        dBound = boundaryPercent * Camera.main.pixelHeight;
-        uBound = Camera.main.pixelHeight - dBound;
+        myCam = GetComponent<Camera>();
+        lBound = boundaryPercent * myCam.pixelWidth;
+        rBound = myCam.pixelWidth - lBound;
+        dBound = boundaryPercent * myCam.pixelHeight;
+        uBound = myCam.pixelHeight - dBound;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        myCam.orthographicSize = (Screen.height / 100f);
+
         if (player)
         {
-            Vector3 spriteLoc = Camera.main.WorldToScreenPoint(player.transform.position);
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, easing) + new Vector3(0, 0, -10);
+           /* Vector3 spriteLoc = myCam.WorldToScreenPoint(player.transform.position);
             Vector3 pos = transform.position;
 
             if (spriteLoc.x < lBound)
@@ -47,7 +52,7 @@ public class FollowCam : MonoBehaviour
             }
 
             pos = Vector3.Lerp(transform.position, pos, easing);
-            transform.position = pos;
+            transform.position = pos;*/
         }
     }
 }
