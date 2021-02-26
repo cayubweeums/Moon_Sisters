982921da318fc2d83d7f10a081213642ea333e2c
Caleb <farmingtoncardinals24@gmail.com>
2021-02-25 20:36:52 -0600

    Background Parallax and Merge Issues Resolved

---
+++ Assets/Scripts/Sources.cs
@@ -0,0 +1,5 @@
+/*https://opengameart.org/content/seamless-cave-background
+ * https://opengameart.org/content/large-forest-background
+ * https://opengameart.org/content/backgrounds-for-2d-platformers
+ * 
+ */
